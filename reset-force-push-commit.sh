dbe8084a9597cf2d8eab678f15736e158848d9d6
the2time-b2b <bearsterns.dev@gmail.com>
2024-01-12 14:01:53 +0530

    reset commit and force push current changes
    The new commit contains the same message of the recent detached commit

---
+++ reset-force-push-commit.sh
@@ -0,0 +1,19 @@
+# Resets to the previous commit and force push any current changes with the same commit message(s).
+resetBranch=$(git log | grep "^commit" -m 2 | tail -n 1 | awk -F " " '{ print $NF }')
+commitLogsLineCount=$(git log | grep "^commit" -m 2 -n | awk -F: '{ print $1 }' | tail -n 1)
+commitMessages=$(git log | grep "^commit" -m 1 -A $(($commitLogsLineCount - 2)) | tail -n +5 | sed 's/^[ \t]*//' | tr -s '\n')
+commitMessagesOverwrite=""
+
+eval git reset $resetBranch
+eval git add .
+
+while IFS= read -r result
+do
+  commitMessagesOverwrite+=" -m \"$result\""
+done < <(echo "$commitMessages")
+
+eval git commit $commitMessagesOverwrite
+
+branchName=$(git branch --list | tail -n +1 | awk -F " " '{ print $NF }')
+eval git push --force origin $branchName
+
